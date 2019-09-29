e7cb03a83c5b6dfeb4b60bf1e7d2a65d8229bbae
desplega <marcel@desplega.com>
2019-09-29 19:02:52 +0200

    Add data functionality

--- server/mqtt/index.js
+++ server/mqtt/index.js
@@ -1,3 +1,4 @@
+var Data = require('../api/data/data.model');
 var mqtt = require('mqtt');
 var config = require('../config/environment');
 
@@ -16,15 +17,32 @@ var client = mqtt.connect({
 client.on('connect', function () {
     console.log('Connected to Mosquitto at ' + config.mqtt.host + ' on port ' + config.mqtt.port);
     client.subscribe('api-engine');
+    client.subscribe('dht11');
 });
 
 client.on('message', function (topic, message) {
     // message is Buffer
+    // console.log('Topic >> ', topic);
+    // console.log('Message >> ', message.toString());
     if (topic === 'api-engine') {
         var macAddress = message.toString();
         console.log('Mac Address >> ', macAddress);
         client.publish('rpi', 'Got Mac Address: ' + macAddress);
+    } else if (topic === 'dht11') {
+        var data = JSON.parse(message.toString());
+        // create a new data record for the device
+        Data.create(data, function (err, data) {
+            if (err) return console.error(err);
+            // if the record has been saved successfully,
+            // websockets will trigger a message to the web-app
+            console.log('Data Saved :', data.data);
+        });
     } else {
         console.log('Unknown topic', topic);
     }
 });
+
+exports.sendLEDData = function (data) {
+    console.log('Sending Data', data);
+    client.publish('led', data);
+}
